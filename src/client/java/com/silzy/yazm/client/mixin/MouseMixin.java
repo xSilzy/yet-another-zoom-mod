ca1d40396622f016ec834a0421d5acecea8faffe
Silzy <153761681+xSilzy@users.noreply.github.com>
2026-08-20 21:00:00 +0200

    Update license

--- src/client/java/com/silzy/yazm/client/mixin/MouseMixin.java
+++ src/client/java/com/silzy/yazm/client/mixin/MouseMixin.java
@@ -1,3 +1,9 @@
+/*
+ * This file is part of the YAZM distribution (https://github.com/xSilzy/yet-another-zoom-mod).
+ * Copyright (c) 2026 xSilzy.
+ * Licensed under GNU GPLv3. See LICENSE for details.
+ */
+
 package com.silzy.yazm.client.mixin;
 
 import net.minecraft.client.Mouse;
